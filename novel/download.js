49c46e21a09a3afa892e255c61a90b6f8185d7b2
rulelee <rulelee@qq.com>
2022-11-05 22:06:37 +0800

    提交代码

--- novel/download.js
+++ novel/download.js
@@ -1,3 +1,17 @@
-window.addEventListener("load", (e) => {
-  console.log(">>>>>>>>>>>>>>>>>>>>> 小说下载");
+window.addEventListener("load", async () => {
+
+  const selectors = {
+    chapters: ".listmain dd+dt~dd a"
+  };
+
+  // 章节列表
+  const chapters = [...document.querySelectorAll(selectors.chapters)].map(elm => {
+    return {
+      name: elm.text,
+      url: elm.href,
+    }
+  });
+
+  console.log(">>>>>>>>>>>>>>>>>>>>> 小说下载", chapters);
+
 });
